70b2d91ce3ece46de67305de93331899aa500a56
unpeng <wangp1982@gmail.com>
2024-06-05 17:22:50 +0800

    add SPM支持

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MJRefreshSwift",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "MJRefreshSwift", targets: ["MJRefreshSwift"]),
+    ],
+    targets: [
+        .target(
+            name: "MJRefreshSwift",
+            path: "JRefresh",
+            resources: [.process("JRefresh.bundle")]
+        ),
+    ]
+)
