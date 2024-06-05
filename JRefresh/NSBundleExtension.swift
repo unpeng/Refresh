118068f24537f42553726dfea6578c2b48d462b4
unpeng <wangp1982@gmail.com>
2024-06-05 20:03:32 +0800

    尝试使用 指定bundle

--- JRefresh/NSBundleExtension.swift
+++ JRefresh/NSBundleExtension.swift
@@ -11,7 +11,7 @@ import UIKit
 public extension Bundle {
     public class func refreshBunle() -> Bundle {
         // 这里不使用mainBundle是为了适配pod 1.x和0.x
-        return Bundle(path: Bundle(for: JRefreshComponent.self).path(forResource: "JRefresh", ofType: "bundle")!)!
+        return Bundle(for: JRefreshComponent.self)
     }
     
     public class func arrowImage() -> UIImage {
